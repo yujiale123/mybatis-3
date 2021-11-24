a7c428b9b6643447bceff7a27e45e38a58b0a5dd
yujiale <yjl@2020>
2021-11-24 16:19:40 +0800

    Mybatis源码解析之Mybatis二级缓存代码实例

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -1,4 +1,5 @@
 import com.mybatis.io.Resource;
+import com.mybatis.pojo.User;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -44,5 +45,28 @@ public class MybatisTest {
     //4、使用jdk动态代理来对mapper接口产生代理对象
     UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
     List<Object> allUser = userMapper.findAllUser();
+  }
+
+  /**
+   * Mybatis二级缓存效果测试
+   *
+   * @throws Exception
+   */
+  public void test03() throws Exception {
+    //1、读取配置文件，读取成字节输入流，还没有解析  Mybatis初始化解析
+    InputStream resourceAsStream = Resource.getResourceAsStream("sqlMapConfig.xml");
+    //2、解析配置文件，把结果封装成Configuration对象，创建DefaultSqlSessionFactory对象
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+    //3、执行SQL流程解析 生产了DefaultSqlSessionFactory对象，设置了事务不自动提交，完成executor对象的创建
+    SqlSession sqlSession1 = sqlSessionFactory.openSession();
+    SqlSession sqlSession2 = sqlSessionFactory.openSession();
+    sqlSession1.commit();
+    User user = new User();
+    user.setId(1);
+    user.setUsername("jack");
+    // 增删改会清空二级缓存
+    sqlSession1.update("com.mybatis.mapper.UserMapper.updateById", user);
+    User user2 = sqlSession2.selectOne("com.mybatis.mapper.UserMapper.findById", 1);
+    System.out.println(user2);
   }
 }
