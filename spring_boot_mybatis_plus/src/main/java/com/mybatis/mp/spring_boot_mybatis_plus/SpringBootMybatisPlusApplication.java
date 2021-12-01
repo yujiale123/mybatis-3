e2c7c9d113071e1e2529783377a0228464d69d0a
yujiale <yjl@2020>
2021-12-01 16:20:01 +0800

    备份

---
+++ spring_boot_mybatis_plus/src/main/java/com/mybatis/mp/spring_boot_mybatis_plus/SpringBootMybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.mybatis.mp.spring_boot_mybatis_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yujiale
+ */
+@SpringBootApplication
+public class SpringBootMybatisPlusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootMybatisPlusApplication.class, args);
+  }
+
+}
